a6e4ef0ca0ce2a8a8b63822faebf98518f28fb56
solarsvxc <hacvuanhtrang.dn@gmail.com>
2025-09-27 21:38:22 +0700

    Assignment 0 init folder project

---
+++ Assignment/S32k144_Assginment_0/src/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-09-26
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "S32K144.h"
+
+
+
+
+int main(void)
+{
+
+}
